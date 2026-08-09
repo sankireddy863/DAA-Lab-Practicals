cad794ff7b42cd362561de3984f3ceec9acdae47
Ankireddy_Sudha <sankireddy863@gmail.com>
2026-08-09 10:32:01 +0530

    Update Linear_searching.py

--- searching/Linear_searching.py
+++ searching/Linear_searching.py
@@ -1,1 +1,21 @@
+#  linear_search
 
+def linear_search_all(arr, target):
+    positions = []
+
+    for i in range(len(arr)):
+        if arr[i] == target:
+            positions.append(i)
+
+    return positions
+
+# Example
+arr = [5, 3, 7, 3, 9, 3]
+target = 3
+
+result = linear_search_all(arr, target)
+
+if result:
+    print("Element found at indices:", result)
+else:
+    print("Element not found")
