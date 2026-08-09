119c5446ae58a25e65ee92ab19ea9c02744a1ba1
Ankireddy_Sudha <sankireddy863@gmail.com>
2026-08-09 10:31:33 +0530

    Update Binary_search.py

--- searching/Binary_search.py
+++ searching/Binary_search.py
@@ -1,1 +1,29 @@
+# binary_search
 
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+
+numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+target = 10
+
+result = binary_search(numbers, target)
+
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
